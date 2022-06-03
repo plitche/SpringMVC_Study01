27f6e5165a1cb8371a5174339774399cb5648cfb
plitche <kysu728@naver.com>
2022-06-03 23:25:53 +0900

    07. 서블릿 - Hello 서블릿

---
+++ Project/servlet/src/main/java/hello/servlet/basic/HelloServlet.java
@@ -0,0 +1,29 @@
+package hello.servlet.basic;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name = "helloServlet", urlPatterns = "/hello")
+// name: 서블릿 이름, urlPatterns: URL 매핑
+// HTTP 요청을 통해 매핑된 URL이 호출되면서 서블릿 컨테이너는 메서드를 실행한다.
+public class HelloServlet extends HttpServlet {
+
+    @Override
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        System.out.println("request = " + request);
+        System.out.println("response = " + response);
+        System.out.println("HelloServlet.service");
+
+        String username = request.getParameter("username");
+        System.out.println("username = " + username);
+
+        response.setContentType("text/plain");
+        response.setCharacterEncoding("utf-8");
+        response.getWriter().write("hello " + username);
+    }
+}
