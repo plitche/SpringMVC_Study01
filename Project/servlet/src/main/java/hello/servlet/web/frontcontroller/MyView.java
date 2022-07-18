797b37f3d8b6467bcd003d49c6ec40864767103d
plitche <kysu728@naver.com>
2022-07-18 23:48:43 +0900

    29. MVC 프레임워크 만들기 - 단순하고 실용적인 컨트롤러_V4

--- Project/servlet/src/main/java/hello/servlet/web/frontcontroller/MyView.java
+++ Project/servlet/src/main/java/hello/servlet/web/frontcontroller/MyView.java
@@ -15,6 +15,9 @@ public class MyView {
         this.viewPath = viewPath;
     }
 
+    public MyView() {
+    }
+
     public void render(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         RequestDispatcher dispatcher = request.getRequestDispatcher(viewPath);
         dispatcher.forward(request, response);
