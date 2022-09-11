d8532bb94c17e918c6dbe7fdc51e10c257ca4d78
plitche <kysu728@naver.com>
2022-09-11 22:56:25 +0900

    65. 웹 페이지 만들기 - RedirectAttributes

--- Project/item-service/src/main/java/hello/itemservice/web/basic/BasicItemController.java
+++ Project/item-service/src/main/java/hello/itemservice/web/basic/BasicItemController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import javax.annotation.PostConstruct;
 import java.util.List;
@@ -87,12 +88,20 @@ public class BasicItemController {
         return "basic/addForm";
     }
 
-    @PostMapping("/add")
+    // @PostMapping("/add")
     public String addItemV5(Item item) {
         // @ModelAttribute 생략 가능
         itemRepository.save(item);
         return "redirect:/basic/items/" + item.getId(); // 한글이나 띄어쓰기, 특수문자가 있으면 인코딩해야한다.
         // return "basic/addForm";
+    }
+
+    @PostMapping("/add")
+    public String addItemV6(Item item, RedirectAttributes redirectAttributes) {
+        Item savedItem = itemRepository.save(item);
+        redirectAttributes.addAttribute("itemId", savedItem.getId());
+        redirectAttributes.addAttribute("status", true);
+        return "redirect:/basic/items/{itemId}";
     }
 
     @GetMapping("/{itemId}/edit")
