03f7630a828a86f7ad108cfff60f46503a7befeb
plitche <kysu728@naver.com>
2022-09-11 22:16:19 +0900

    63. 웹 페이지 만들기 - 상품 수정

--- Project/item-service/src/main/java/hello/itemservice/web/basic/BasicItemController.java
+++ Project/item-service/src/main/java/hello/itemservice/web/basic/BasicItemController.java
@@ -87,6 +87,20 @@ public class BasicItemController {
         return "basic/addForm";
     }
 
+    @GetMapping("/{itemId}/edit")
+    public String editFrom(@PathVariable Long itemId, Model model) {
+        Item item = itemRepository.findById(itemId);
+        model.addAttribute("item", item);
+        return "basic/editForm";
+    }
+
+    @PostMapping("/{itemId}/edit")
+    public String edit(@PathVariable Long itemId, @ModelAttribute Item item) {
+        itemRepository.update(itemId, item);
+
+        return "redirect:/basic/{itemId}";
+    }
+
     /**
     * 테스트용 데이터 추가
     */
